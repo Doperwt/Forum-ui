e96fe31a7c5bf1587685d59a58e6666e7560fb59
doperwt <r.f.denouden@gmail.com>
2018-03-13 16:15:16 +0100

    adding dependancies folders starting on nav bar

---
+++ src/components/ui/nav.js
@@ -0,0 +1,38 @@
+import React, { PureComponent } from 'react'
+import { push } from 'react-router-redux'
+import PropTypes from 'prop-types'
+
+class Nav extends PureComponent {
+  static propTypes = {
+    signedIn: PropTypes.bool.isRequired,
+    push: PropTypes.func.isRequired,
+    signOut: PropTypes.func.isRequired,
+  }
+
+  goHome = () => {
+    this.props.push('/')
+  }
+
+  signOut = (event) => {
+  event.preventDefault()
+  this.props.signOut()
+  }
+
+  signIn = () => {
+    this.props.push('/sign-in')
+  }
+
+  render(){
+    return(
+      <div>
+
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = ({ currentUser }) => ({
+  signedIn: (!!currentUser && !!currentUser._id)
+})
+
+export default connect(mapStateToProps, { push, signOut })(Nav)
