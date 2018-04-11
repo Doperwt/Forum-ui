93797bb54b50c0cb2b314ed5ec01f550c019e963
doperwt <r.f.denouden@gmail.com>
2018-04-11 15:51:29 +0200

    prepping for heroku deploy

--- src/api/client.js
+++ src/api/client.js
@@ -7,7 +7,7 @@ export default class ApiClient {
 
   constructor(host, options = {}) {
     this.host = process.env.NODE_ENV === 'production'
-      ? 'https://expert-chinbone.codaisseur.cloud' // WITHOUT the / !!!
+      ? process.env.BACKEND_URL // WITHOUT the / !!!
       : (host || 'http://localhost:3030')
 
     this.options = { ...this.defaultOptions, ...options }
