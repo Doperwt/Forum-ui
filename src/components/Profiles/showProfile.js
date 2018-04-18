826ad28259976df4a699381af8cb9788b97ac6a3
doperwt <r.f.denouden@gmail.com>
2018-04-18 12:13:18 +0200

    profile cleanup, image sizing

--- src/components/Profiles/showProfile.js
+++ src/components/Profiles/showProfile.js
@@ -2,7 +2,7 @@ import React, { PureComponent } from 'react'
 import { push } from 'react-router-redux'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
-import { updateProfile,getProfile } from '../../actions/user'
+import { getProfile } from '../../actions/user'
 import { connect as subscribeToWebsocket } from '../../actions/websocket'
 import noPic from '../../lib/GenericImages/118781.png'
 
@@ -10,7 +10,6 @@ class ShowProfile extends PureComponent {
   static propTypes = {
     push: PropTypes.func.isRequired,
     getProfile: PropTypes.func.isRequired,
-    updateProfile: PropTypes.func.isRequired,
     signedIn: PropTypes.bool
   }
   componentWillMount() {
@@ -18,51 +17,21 @@ class ShowProfile extends PureComponent {
     if (!signedIn) push('/sign-in')
     if(!profile){
       getProfile(userId)
-    } else {
-      if(!!profile.fullName){
-        this.setState({
-          firstName: profile.fullName.split(' ')[0],
-          lastName:profile.fullName.split(' ').splice(1).join(' '),
-          bio: profile.bio,
-          picture:profile.picture,
-        })
-      }
     }
     subscribeToWebsocket()
-  }
-  componentWillReceiveProps(){
-    const { profile } = this.props
-    if(!!profile&&!!profile.fullName){
-      this.setState({
-        firstName: profile.fullName.split(' ')[0],
-        lastName:profile.fullName.split(' ').splice(1).join(' '),
-        bio: profile.bio,
-        picture:profile.picture,
-      })
-    }
-  }
-  constructor(props) {
-    super(props)
-    this.state = {
-      firstName: '',
-      lastName:'',
-      bio: "Write something 'interesting' about yourself ",
-      picture:'',
-      editHidden:true,
-    }
   }
 
   render(){
     const profile = this.props.profile
     return(
       <div>
-        <img src={!!profile? (!!profile.picture?profile.picture:noPic):noPic} alt={!!profile?profile.fullName:'no name'} />
+        <img className='profile_pic' src={!!profile? (!!profile.picture?profile.picture:noPic):noPic} alt={!!profile?profile.fullName:'no name'} />
         <div className='input'>
           <p>{!!profile ? profile.fullName:'Your full name'}</p>
         </div>
         <div className='input' >
           <hr />
-          <p>{!!profile ? profile.bio:this.state.bio}</p>
+          <p>{!!profile ? profile.bio:'this.state.bio'}</p>
         </div>
     </div>
     )
@@ -77,4 +46,4 @@ const mapStateToProps = ({ currentUser, profile }) => {
   }
 }
 
-export default connect(mapStateToProps, { push,getProfile,updateProfile })(ShowProfile)
+export default connect(mapStateToProps, { push,getProfile })(ShowProfile)
